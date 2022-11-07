9bfa02e55a4f31fe0ad5afb04d6a759a0a96b92b
prateek <prateek150889@gmail.com>
2022-11-07 18:05:35 +0530

    changed program

--- AllsubstringsOfAstring.py
+++ AllsubstringsOfAstring.py
@@ -8,7 +8,7 @@ for i in range(len(string1)+1):
         if string1[i:j] not in list1:
             list1.append(string1[i:j])
 
-print list1
+print (list1)
 
 '''
 Given a string s, check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
@@ -21,9 +21,10 @@ Explanation: It is the substring "ab" twice.
 
 '''
 
-
+'''
 for item in list1:
     for i in range(1,len(string1)):
         if item*i == string1:
-            print "yes the string '",string1, "' can be made after" ,i, "iterations of adding '", item, "'"
+            print ("yes the string '",string1, "' can be made after" ,i, "iterations of adding '", item, "'")
             break
+'''
